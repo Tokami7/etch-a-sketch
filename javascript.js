0b6162c8e5c65fbd0aba635ec2ac9dcf9845da53
Tom <thomas.mant@hotmail.com>
2026-05-21 19:51:02 +0100

    Rezize sketch box, create ui and res/clear buttons

--- javascript.js
+++ javascript.js
@@ -1,18 +1,41 @@
 const container = document.querySelector("#container");
 const squares = document.querySelectorAll(".column");
-const rows = 16;
-const columns = 16;
+const sketch = document.querySelector("#sketch");
+const leftButton = document.querySelector(".left-button");
+const rightButton = document.querySelector(".right-button");
+let size = 40;
 
-for (let i = 0; i < rows; i++) {
-    const row = document.createElement("div");
-    row.className = "row";
-    for (let j = 0; j < columns; j++) {
-        const column = document.createElement("div");
-        column.className = "column";
-        column.style.border = "2px solid black";
-        column.addEventListener("mouseenter", (e) => e.target.style.backgroundColor = "gray");
-        row.appendChild(column);
+
+function createGrid() {
+
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement("div");
+        row.className = "row";
+        for (let j = 0; j < size; j++) {
+            const column = document.createElement("div");
+            column.className = "column";
+            column.style.border = "1px solid black";
+            column.addEventListener("mouseenter", (e) => e.target.style.backgroundColor = "gray");
+            row.appendChild(column);
+        };
+        sketch.appendChild(row);
     };
-    container.appendChild(row);
 };
 
+leftButton.addEventListener("click", () => {
+    let input = Number(prompt("Enter a grid size (max 100)", 16))
+    if (input <= 100 && input > 0) {
+        sketch.innerHTML = "";
+        size = input;
+        createGrid();
+    } else {
+        alert("Invalid size");
+    };
+});
+
+rightButton.addEventListener("click", () => {
+    sketch.innerHTML = "";
+    createGrid();
+});
+
+createGrid();
